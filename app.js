a0c51462907fc0b1955457be8df4124249e054a4
mogox95123 <154760105+mogox95123@users.noreply.github.com>
2024-01-17 16:18:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('admin/index', { title: 'Home' });
 });
 
 
