e269a7a9fff9b0975be5e59c2fb5980936efb5d4
wghari <1786126@crosemont.qc.ca>
2024-01-25 23:09:06 -0500

    nothing

--- routers/atb.js
+++ routers/atb.js
@@ -31,7 +31,6 @@ router.get('/details', async (req, res) => {
                     countRmvDetailsPage++;
                 } else {
                     countRmvCardPage++;
-                    //console.log(countRmvCardPage);
                 }
             }
         });
